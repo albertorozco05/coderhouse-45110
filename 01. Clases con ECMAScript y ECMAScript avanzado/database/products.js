d0b4adf6216751425288e9ae04b9b999790b5ae3
Nahuel Gómez <ngomezpf@gmail.com>
2023-02-02 19:06:39 -0300

    First challenge

---
+++ 01. Clases con ECMAScript y ECMAScript avanzado/database/products.js
@@ -0,0 +1,4 @@
+const bebida = { title: "Coca Cola", description: "Bebida gaseosa", price: 100, thumbnail: "https://s3-symbol-logo.tradingview.com/coca-cola--600.png", code: 1, stock: 110 };
+const zapato = { title: "Zapato", description: "Zapato de cuero", price: 400, thumbnail: "https://images.pexels.com/photos/292999/pexels-photo-292999.jpeg", code: 1, stock: 30 };
+
+module.exports = { bebida, zapato };
